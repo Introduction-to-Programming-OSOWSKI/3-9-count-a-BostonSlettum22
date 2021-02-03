79026bd2c435766dab162c00e553cc8c0bb9c95c
BostonSlettum22 <816661@mystma.org>
2021-02-03 00:26:25 +0000

    countA 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    bazinga = 0
+
+    for i in range(0,len(w)):
+        bazinga = bazinga + i
+        if bazinga == len("a"):
+            return len("a")
+
+print(countA("bazinga"))
+
